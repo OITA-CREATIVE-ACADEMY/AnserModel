5a92a26d7cc68da16fccb329ef8bfbc885f933d0
tri-tanaka <tanaka@triana.jp>
2019-09-09 14:35:18 +0900

    基本演習@文字列操作 回答追加

---
+++ basic_practice/string_operation.rb
@@ -0,0 +1,16 @@
+#1
+puts "りんご"
+#2
+my_name = "山田太郎"
+puts "わたしの名前は#{my_name}です。"
+#3
+puts "日本の首都は" + "東京です。"
+#4
+fruits = "りんご,ぶどう,もも"
+puts fruits.split(",")
+#5
+string = "supercalifragilisticexpialidocious"
+puts string.length()
+#6
+string = "supercalifragilisticexpialidocious"
+puts string.count("i")
